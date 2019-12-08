34d77cc3ef41370a4ebba94db68b4d40bb2c8482
lawwman <lawrenciok@gmail.com>
2019-12-08 23:02:07 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const { src, dest, watch, series} = require("gulp");
+var browserSync = require("browser-sync").create();
+var sass = require("gulp-sass");
+
+
+function convert_sass() {
+    return src('src/scss/*.scss').pipe(sass()).pipe(dest('src/css')) .pipe(browserSync.stream());;
+}
+
+function serve() {
+    browserSync.init({
+        server: "./src"
+    });
+
+    watch('src/scss/*.scss', series(convert_sass));
+    watch('src/index.html').on("change", browserSync.reload);
+}
+
+exports.convert_sass = convert_sass;
+exports.default = serve;
