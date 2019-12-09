7e6de5cdfdbe788de083de1dc98b8f2a9c42bf03
lawwman <lawrenciok@gmail.com>
2019-12-10 00:17:09 +0800

    encapsulate trip settings differently. auto display trip duration

--- src/events.js
+++ src/events.js
@@ -1,36 +1,3 @@
-// Add listener to create event form
-document.getElementById("create_event").addEventListener('submit', add_event);
-
-// Add function to save changes to trip settings
-function saveChangesToTripSettings() {
-
-    // Handle date
-    if (document.getElementById('startDate').value == "" || document.getElementById('endDate').value == "") alert("Please fill in both start and end date.");
-
-    else {
-        var start_date = document.getElementById('startDate').value.split("-");
-        var start_year = start_date[0];
-        var start_month = start_date[1];
-        var start_day = start_date[2];
-        var end_date = document.getElementById('endDate').value.split("-");
-        var end_year = end_date[0];
-        var end_month = end_date[1];
-        var end_day = end_date[2];
-
-        if (start_year > end_year) alert("Please make sure year of start date is before year of end date.");
-        else if (start_month > end_month) alert("Please make sure month of start date is before month of end date.");
-        else if (start_day > end_day) alert("Please make sure day of start date is before day of end date.");
-        else {
-            var trip_duration = start_date + "-" + end_date;
-            window.localStorage.setItem("trip_duration", trip_duration);
-        }
-    }
-}
-
-function clearAllSettings() {
-    window.localStorage.clear();
-}
-
 // Add new event
 function add_event(e) {
 
@@ -46,3 +13,6 @@ function add_event(e) {
 $(window).on('load',function(){
     $('#myModal').modal('show');
 });
+
+// Add listener to create event form
+document.getElementById("create_event").addEventListener('submit', add_event);
---
+++ src/trip_settings.js
@@ -0,0 +1,44 @@
+// Add function to save changes to trip settings
+function saveChangesToTripSettings() {
+
+    // Handle date
+    if (document.getElementById('startDate').value == "" || document.getElementById('endDate').value == "") alert("Please fill in both start and end date.");
+
+    else {
+        var start_date = document.getElementById('startDate').value.split("-");
+        var start_year = start_date[0];
+        var start_month = start_date[1];
+        var start_day = start_date[2];
+        var end_date = document.getElementById('endDate').value.split("-");
+        var end_year = end_date[0];
+        var end_month = end_date[1];
+        var end_day = end_date[2];
+
+        if (start_year > end_year) alert("Please make sure year of start date is before year of end date.");
+        else if (start_month > end_month) alert("Please make sure month of start date is before month of end date.");
+        else if (start_day > end_day) alert("Please make sure day of start date is before day of end date.");
+        else {
+            var trip_duration = document.getElementById('startDate').value + "_" + document.getElementById('endDate').value;
+            window.localStorage.setItem("trip_duration", trip_duration);
+        }
+    }
+}
+
+function displayTripSettings() {
+    console.log("hello")
+    if (localStorage.getItem("trip_duration") != null) {
+
+        var trip_duration = localStorage.getItem("trip_duration").split("_");
+        var start_date = trip_duration[0];
+        var end_date = trip_duration[1];
+
+        document.querySelector("#startDate").value = start_date;
+        document.querySelector("#endDate").value = end_date;
+    }
+}
+
+function clearAllSettings() {
+    window.localStorage.clear();
+}
+
+displayTripSettings();
