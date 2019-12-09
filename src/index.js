584e3d65c89b6de7b6133eee53ffebfd4ab9125f
lawwman <lawrenciok@gmail.com>
2019-12-09 23:45:46 +0800

    add function to clear local storage and set trip duration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 $(window).on('load',function(){
+
+    // check local storage if trip duration is set
+    if (localStorage.getItem("trip_duration") == null) {
+        console.log("duration not set.");
+        $('#schedule').hide();
+    } else {
+        $('#no_duration_warning').hide();
+    }
+
     $('#myModal').modal('show');
-});
+}); 
